43b3dbd86a8f08e18d134056e167fd095c464035
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2020-12-06 14:57:01 +0000

    Add CV page

---
+++ src/pages/cv.js
@@ -0,0 +1,25 @@
+import Typography from '@material-ui/core/Typography'
+
+import Comments from '@Components/common/Comments'
+import Title from '@Components/common/Title'
+
+export default () => (
+    <div>
+        <Title title="CV" />
+        <br />
+        <Typography variant="body1">
+            Download my CV{' '}
+            <a href="https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.pdf">
+                here
+            </a>
+            . If you like my CV and would like to see the source code, you can
+            download my repo{' '}
+            <a href="https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.tar.gz">
+                here
+            </a>
+            . You can build the latex file with the command `lualatex
+            ajayakumar_vipin_cv.tex`.
+        </Typography>
+        <Comments.Embed id="cv" />
+    </div>
+)
