5ffae563c12fcc5d01fedd070b4f9bee5225d930
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2020-12-25 20:42:58 +0000

    Tidy up Youtube component

--- src/components/blog/Youtube.js
+++ src/components/blog/Youtube.js
@@ -1,13 +1,35 @@
-export default ({ id }) => (
-    <div className="youtube-wrapper">
-        <iframe
-            title={id}
-            width="560"
-            height="315"
-            src={`https://www.youtube.com/embed/${id}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-        />
-    </div>
-)
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+    youtubeWrapper: {
+        position: 'relative',
+        width: '100%',
+        height: 0,
+        paddingBottom: '56.25%',
+        '& iframe': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            border: 0
+        }
+    }
+}))
+
+export default ({ id }) => {
+    const classes = useStyles()
+    return (
+        <div className={classes.youtubeWrapper}>
+            <iframe
+                title={id}
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${id}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            />
+        </div>
+    )
+}
