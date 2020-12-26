734ae8f9d7f99e57e930cb6440ace7a51f853e82
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2020-12-26 21:14:21 +0000

    Reverse proxy cv

--- next.config.js
+++ next.config.js
@@ -5,3 +5,20 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
 })
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/cv/pdf',
+                destination:
+                    'https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.pdf'
+            },
+            {
+                source: '/cv/source',
+                destination:
+                    'https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.tar.gz'
+            }
+        ]
+    }
+}
--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,27 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Typography from '@material-ui/core/Typography'
+import Link from 'next/link'
 
 import Comments from '@Components/Comments'
 import Title from '@Components/Title'
 import StandardSeo from '@Components/StandardSeo'
 
-export default () => (
+const CV = () => (
     <>
         <StandardSeo pageTitle="CV" description="My CV" path="/cv" />
         <Title title="CV" />
         <br />
         <Typography variant="body1">
             Download my CV{' '}
-            <a href="https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.pdf">
-                here
-            </a>
+            <Link href="/cv/pdf">
+                <a>here</a>
+            </Link>
             . If you like my CV and would like to see the source code, you can
             download my repo{' '}
-            <a href="https://bluprince13.gitlab.io/cv/ajayakumar_vipin_cv.tar.gz">
-                here
-            </a>
+            <Link href="/cv/source">
+                <a>here</a>
+            </Link>
             . You can build the latex file with the command `lualatex
             ajayakumar_vipin_cv.tex`.
         </Typography>
         <Comments.Embed id="cv" />
     </>
 )
+
+export default CV
