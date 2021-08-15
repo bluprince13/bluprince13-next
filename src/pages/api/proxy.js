de191dd03bf8db919049f08601f6b316292d3b87
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2021-08-15 10:57:46 +0100

    Fix proxying

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,13 +1,35 @@
+// https://vercel.com/support/articles/how-to-enable-cors
+
 import fetch from 'isomorphic-unfetch'
 
-export default async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    // eslint-disable-next-line consistent-return, no-return-await
+    return await fn(req, res)
+}
+
+const handler = async (req, res) => {
     const { url } = req.query
 
     try {
         const response = await fetch(url).then((x) => x.json())
         return res
             .status(200)
-            .header('Access-Control-Allow-Origin', 'https://bluprince13.com')
             .json(response)
     } catch (error) {
         return res
@@ -15,3 +37,5 @@ export default async (req, res) => {
             .json({ error: error.message || error.toString() })
     }
 }
+
+module.exports = allowCors(handler)
