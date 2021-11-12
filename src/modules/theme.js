8e39855d330de783b228d453aa801c17b20bb173
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2021-11-12 21:36:15 +0000

    MaterialUI migration from v4 to v5 - Remove adaptV4Theme

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,13 +1,13 @@
-import { createTheme, adaptV4Theme } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles'
 
-const theme = createTheme(adaptV4Theme({
+const theme = createTheme({
     palette: {},
     apps: {
         spanish: {
             columnWidth: '25%'
         }
     }
-}))
+})
 
 theme.typography.h1.fontSize = '4rem'
 
