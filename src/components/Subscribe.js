5bd13df8c7a480e551d411dee638470b1b29babd
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2023-04-24 19:52:38 +0000

    Update subscribe modal

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -17,9 +17,6 @@ const useStyles = makeStyles(() => ({
         padding: '0.5rem',
         borderRadius: 10,
         margin: "1rem 0"
-    },
-    subscribeButton: {
-        marginLeft: '1rem'
     }
 }))
 
@@ -27,76 +24,24 @@ const useStyles = makeStyles(() => ({
 function Subscribe() {
     const classes = useStyles()
 
-    const { data } = useSWR('/api/buttondown/subscribers', fetcher)
-    const subscriberCount = format(data?.count)
-
-    const inputEl = useRef(null)
-    const [message, setMessage] = useState('')
-
-    const subscribe = async (e) => {
-        e.preventDefault()
-
-        const res = await fetch('/api/buttondown/subscribe', {
-            body: JSON.stringify({
-                email: inputEl.current.value
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'POST'
-        })
-
-        const { error } = await res.json()
-
-        if (error) {
-            setMessage(error)
-            return
-        }
-
-        inputEl.current.value = ''
-        setMessage('Success! 🎉 You are now subscribed.')
-    }
-
     return (
         <Box className={classes.root} bgcolor={blue[50]}>
-            <form onSubmit={subscribe} className={classes.root}>
+            <div className={classes.root}>
                 <div>
                     <Typography variant="overline">
                         Get emails from me about new articles{' '}
                     </Typography>
                     <Emoji symbol="📮" label="subscribe" />
                 </div>
-                <Box>
-                    <Box display="flex">
-                        <Box flexGrow={1}>
-                            <TextField
-                                placeholder="hero@awesome.com"
-                                variant="standard"
-                                type="email"
-                                inputRef={inputEl}
-                                fullWidth
-                                required
-                                InputLabelProps={{ required: false }}
-                            />
-                        </Box>
-                        <Button
-                            className={classes.subscribeButton}
-                            variant="contained"
-                            type="submit"
-                            color="primary"
-                        >
-                            Subscribe
-                        </Button>
-                    </Box>
-                </Box>
-                <div>
-                    {message || `${subscriberCount || '---'} subscribers`}
+                <div style={{ marginTop: '1rem' }}>
+                    Subscribe to email via {' '}
+                    <a href="https://buttondown.email/bluprince13">buttondown.email/bluprince13</a>.
                 </div>
                 <div style={{ marginTop: '1rem' }}>
                     Prefer RSS? Subscribe to my{' '}
                     <a href="https://bluprince13.com/feed.xml">RSS feed</a>.
                 </div>
-            </form>
+            </div>
         </Box>
     )
 }
