63af5cc77d6afda9babc1ad20728772177b55079
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2024-05-25 12:33:58 +0100

    Remove MUI and upgrade Hyvor

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,10 @@
-import HyvorTalk from 'hyvor-talk-react'
+import { Comments, CommentCount } from '@hyvor/hyvor-talk-react';
 
 const HYVOR_WEBSITE_ID = 2205
 
 export default {
     CommentCount: ({ id }) => (
-        <HyvorTalk.CommentCount websiteId={HYVOR_WEBSITE_ID} id={id} />
+        <CommentCount website-id={HYVOR_WEBSITE_ID} page-id={id} />
     ),
-    Embed: ({ id }) => <HyvorTalk.Embed websiteId={HYVOR_WEBSITE_ID} id={id} />
+    Embed: ({ id }) => <Comments website-id={HYVOR_WEBSITE_ID} page-id={id} />
 }
