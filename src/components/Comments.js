d6cbb5f32333e054ddef6198de98623dfe84c183
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2024-05-27 12:12:28 +0100

    Add some margin for Comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { Comments, CommentCount } from '@hyvor/hyvor-talk-react'
+import { Box } from '@mui/material'
 
 const HYVOR_WEBSITE_ID = 2205
 
@@ -9,5 +10,7 @@ export const MyCommentCount = ({ id }) => (
 )
 
 export const MyComments = ({ id }) => (
-    <Comments website-id={HYVOR_WEBSITE_ID} page-id={id} />
+    <Box sx={{ mt: 2 }}>
+        <Comments website-id={HYVOR_WEBSITE_ID} page-id={id} />
+    </Box>
 )
