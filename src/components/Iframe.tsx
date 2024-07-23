8b24cdd353cb9f8cf8acd6bb0350c00f2237ef3f
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2024-07-23 07:11:52 +0100

    Switch iframe to static for mobile

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -1,32 +1,40 @@
 import * as React from 'react'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import Figure from './Figure'
 
 interface IframeProps {
     src: string
+    staticSrc: string
 }
 
-export const Iframe: React.FC<IframeProps> = ({
-    src
-}) => (
-    <div
-        style={{
-            position: 'relative',
-            overflow: 'hidden',
-            width: '100%',
-            paddingTop: '56.25%'
-        }}
-    >
-        <iframe
+export const Iframe: React.FC<IframeProps> = ({ src, staticSrc }) => {
+    const isMobile = useMediaQuery('(max-width:600px)')
+
+    return isMobile && staticSrc ? (
+        <Figure src={staticSrc} size="l" />
+    ) : (
+        <div
             style={{
-                border: 0,
-                position: 'absolute',
-                top: '0',
-                bottom: '0',
-                left: '0',
-                right: '0',
+                position: 'relative',
+                overflow: 'hidden',
                 width: '100%',
-                height: '100%'
+                paddingTop: '56.25%',
+                minWidth: '300px'
             }}
-            src={src}
-        ></iframe>
-    </div>
-)
+        >
+            <iframe
+                style={{
+                    border: 0,
+                    position: 'absolute',
+                    top: '0',
+                    bottom: '0',
+                    left: '0',
+                    right: '0',
+                    width: '100%',
+                    height: '100%'
+                }}
+                src={src}
+            ></iframe>
+        </div>
+    )
+}
