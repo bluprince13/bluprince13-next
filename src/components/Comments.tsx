1c0343caaa837008c056b529eaa473ab606bd091
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2026-05-20 20:17:46 +0100

    Fix comment counts

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,15 +1,21 @@
 'use client'
 
+import { useEffect } from 'react'
 import { Comments, CommentCount } from '@hyvor/hyvor-talk-react'
+import { CommentCounts } from '@hyvor/hyvor-talk-base'
 import { Box } from '@mui/material'
 import { useColorScheme } from '@mui/material/styles'
 import { useSyncExternalStore } from 'react'
 
 const HYVOR_WEBSITE_ID = 2205
 
-export const MyCommentCount = ({ id }: { id: string }) => (
-    <CommentCount website-id={HYVOR_WEBSITE_ID} page-id={id} />
-)
+export const MyCommentCount = ({ id }: { id: string }) => {
+    useEffect(() => {
+        CommentCounts.load({ 'website-id': HYVOR_WEBSITE_ID })
+    }, [])
+
+    return <CommentCount website-id={HYVOR_WEBSITE_ID} page-id={id} />
+}
 
 export function MyComments({ id }: { id: string }) {
     const { mode } = useColorScheme()
